d359053384738c5157ca5ebd501a3f10ae2bf7eb
Balamanikanta Tirunagaram <balaaa@Balamanikantas-MacBook-Air.local>
2026-08-20 02:47:25 +0530

    Revert "feat(web-admin): add landing homepage with routing"
    This reverts commit 0546b068bc7ceaf64a693b9ffb7fb63e0eaeb209.

--- web-admin/src/app/(landing)/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import About from "@/components/landing/About";
-
-export default function AboutPage() {
-  return (
-    <div className="pt-12 pb-12">
-      <About />
-    </div>
-  );
-}
--- web-admin/src/app/(landing)/our-team/page.tsx
+++
@@ -1,5 +0,0 @@
-import Team from "@/components/landing/Team";
-
-export default function OurTeamPage() {
-  return <Team />;
-}
