ebabbdacab4cd36c20a31f00a2a23530dcc5adcc
Balamanikanta Tirunagaram <balaaa@Balamanikantas-MacBook-Air.local>
2026-08-20 11:14:43 +0530

    refactor: remove Incubation Pipeline preview from homepage

--- edc-website/src/app/(landing)/page.tsx
+++ edc-website/src/app/(landing)/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/landing/Hero";
-import StartupFlowPreview from "@/components/landing/StartupFlowPreview";
 import Features from "@/components/landing/Features";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-      <StartupFlowPreview />
       <Features />
     </>
   );
